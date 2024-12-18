f3db56aac729a348f8a5f106b0b5779b68346d98
hanseounggyun <hanseu9839@gmail.com>
2024-12-18 15:31:31 +0900

    Proxy Spring

--- src/main/java/hello/proxy/ProxyApplication.java
+++ src/main/java/hello/proxy/ProxyApplication.java
@@ -4,6 +4,8 @@ import hello.proxy.config.AppV1Config;
 import hello.proxy.config.AppV2Config;
 import hello.proxy.config.v1_proxy.ConcreteProxyConfig;
 import hello.proxy.config.v1_proxy.InterfaceProxyConfig;
+import hello.proxy.config.v2_proxy.DynamicProxyBasicConfig;
+import hello.proxy.config.v2_proxy.DynamicProxyFilterConfig;
 import hello.proxy.trace.logtrace.LogTrace;
 import hello.proxy.trace.logtrace.ThreadLocalLogTrace;
 import org.springframework.boot.SpringApplication;
@@ -14,8 +16,9 @@ import org.springframework.context.annotation.Import;
 //@Import(AppV1Config.class)
 //@Import({AppV1Config.class, AppV2Config.class})
 //@Import(InterfaceProxyConfig.class)
-@Import(ConcreteProxyConfig.class)
-@SpringBootApplication(scanBasePackages = "hello.proxy.config.v1_proxy") //주의
+//@Import(DynamicProxyBasicConfig.class)
+@Import(DynamicProxyFilterConfig.class)
+@SpringBootApplication(scanBasePackages = "hello.proxy.config.v2_proxy") //주의
 public class ProxyApplication {
 
 	public static void main(String[] args) {
